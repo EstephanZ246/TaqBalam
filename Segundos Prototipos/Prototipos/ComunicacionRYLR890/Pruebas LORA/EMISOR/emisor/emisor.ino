e4702696374b15a8118ac1b403b182ed30f9fe08
Estephan2001 <68045755+EstephanZ246@users.noreply.github.com>
2022-08-16 12:01:34 -0600

    160822

--- Segundos Prototipos/Prototipos/ComunicacionRYLR890/Pruebas LORA/EMISOR/emisor/emisor.ino
+++ Segundos Prototipos/Prototipos/ComunicacionRYLR890/Pruebas LORA/EMISOR/emisor/emisor.ino
@@ -1,5 +1,16 @@
 int contador;
 
+int tiempo = 0;
+int horas = 1;
+int mins = 10;
+int seg = 20;
+char buf[32];
+
+int temp1, temp2, temp3;
+int Nivel_Bateria;
+int Pedal_Derecho;
+int Velocidad, RPM;
+
 void setup()
 {
   // put your setup code here, to run once:
@@ -9,26 +20,82 @@ void setup()
 
 void loop()
 {
-  delay(1000);
-  contador++;
-  if (contador >= 2000) {
-    contador = 0;
+
+  tiempo = millis() / 1000;
+  horas = tiempo / 3600;
+  mins = tiempo / 60;
+  seg = tiempo - (horas * 3600 + mins * 60);
+
+
+  // temperaturas
+  if (temp1 >= 100) {
+    temp1 = 0;
+  } else {
+    temp1 = temp1 + 10;
   }
-  Serial.println(contador);
 
-  String contval = String(contador);
-  String cmd = "AT+SEND=0," + String(contval.length()) + "," + String(contval) + "\r";
+  if (temp2 >= 100) {
+    temp2 = 0;
+  } else {
+    temp2 = temp2 + 20;
+  }
 
-  
-  //Serial.println("AT+SEND=0,3,val");
-  
+  if (temp3 >= 100) {
+    temp3 = 0;
+  } else {
+    temp3 = temp3 + 15;
+  }
+
+  // Bateria
+  if (Nivel_Bateria >= 100) {
+    Nivel_Bateria = 0;
+  } else {
+    Nivel_Bateria = Nivel_Bateria + 20;
+  }
+
+  // Pedal
+  if (Pedal_Derecho >= 100) {
+    Pedal_Derecho = 0;
+  } else {
+    Pedal_Derecho = Pedal_Derecho + 10;
+  }
+
+  // Velocidad y RPM
+  if (Velocidad >= 40) {
+    Velocidad = 0;
+    RPM = 0;
+  } else {
+    Velocidad = Velocidad + 3;
+    RPM = RPM + 5;
+  }
+
+
+  String temps = String(temp1) + 'Q' + String(temp2)+ 'W' + String(temp3)+ 'T' + String(Nivel_Bateria)+ 'Y' + String(Pedal_Derecho);
+  String cmd = "AT+SEND=0," + String(temps.length()) + ",B" + temps + "\r";
   Serial2.println(cmd);
+  delay(10);
 
-  
-  while (Serial2.available()) {
-    Serial.write(Serial2.read());
-  }
-  Serial.println();
-  Serial.println(cmd);
-  delay(50);
+  /*temps = String(temp2);
+  cmd = "AT+SEND=0,"  + String(temps.length())  + ",Q" + temps + "\r";
+  Serial2.println(cmd);
+  delay(10);
+
+  temps =  String(temp3);
+  cmd = "AT+SEND=0,"  + String(temps.length()) + ",W" + temps + "\r";
+  Serial2.println(cmd);
+  delay(10);
+
+  temps =  String(Nivel_Bateria);
+  cmd = "AT+SEND=0,"  + String(temps.length())  + ",T" + temps + "\r";
+  Serial2.println(cmd);
+  delay(10);
+
+  temps =  String(Pedal_Derecho);
+  cmd = "AT+SEND=0,"   + String(temps.length())  + ",Y" + temps + "\r";
+  Serial2.println(cmd);
+  delay(10);*/
+
+
+
+
 }
--- Segundos Prototipos/Prototipos/ComunicacionRYLR890/Pruebas LORA/RECEPTOR/receptor/receptor.ino
+++ Segundos Prototipos/Prototipos/ComunicacionRYLR890/Pruebas LORA/RECEPTOR/receptor/receptor.ino
@@ -1,29 +1,135 @@
+int tiempo = 0;
+int horas = 1;
+int mins = 10;
+int seg = 20;
+char buf[32];
+
+int temp1, temp2, temp3;
+int Nivel_Bateria;
+int Pedal_Derecho;
+int Velocidad, RPM;
+
+
 void setup()
 {
   // put your setup code here, to run once:
-  Serial.begin(115200);
+  Serial.begin(9600);
   Serial2.begin(115200);
 
- 
+  delay(1000);
+  Serial.print("page LOGOS");
+  Serial.write(0xff);
+  Serial.write(0xff);
+  Serial.write(0xff);
+  delay(2000);
+  Serial.print("page DATOS");
+  Serial.write(0xff);
+  Serial.write(0xff);
+  Serial.write(0xff);
+  delay(2000);
+  Serial.print("t0.txt=\"ON\"");
+  Serial.write(0xff);
+  Serial.write(0xff);
+  Serial.write(0xff);
+
+
 }
- 
+
 void loop()
 {
+
+  tiempo = millis() / 1000;
+  horas = tiempo / 3600;
+  mins = tiempo / 60;
+  seg = tiempo - (horas * 3600 + mins * 60);
+
   //char ch;
   String inString;
   while (Serial2.available())
   {
-    if(Serial2.available()){
-    inString += String(char(Serial2.read()));
+    if (Serial2.available()) {
+      //inString += String(char(Serial2.read()));
+      if (Serial2.read() == 'B') {
+        inString = Serial2.readStringUntil('Q');
+        temp1 = inString.toInt();
+
+      }
+      else if (Serial2.read() == 'Q') {
+        inString = Serial2.readStringUntil('W');
+        temp2 = inString.toInt();
+
+      }
+      else if (Serial2.read() == 'W') {
+        inString = Serial2.readStringUntil('T');
+        temp3 = inString.toInt();
+
+      }
+      else if (Serial2.read() == 'T') {
+        inString = Serial2.readStringUntil('Y');
+        Nivel_Bateria = inString.toInt();
+
+      }
+      else if (Serial2.read() == 'Y') {
+        inString = Serial2.readStringUntil('\r');
+        Pedal_Derecho = inString.toInt();
+
+      }
+
+
     }
+    Serial.print("Temp1: ");
+      Serial.println(temp1);
+      Serial.print("Temp2: ");
+      Serial.println(temp2);
+      Serial.print("Temp3: ");
+      Serial.println(temp3);
+      Serial.print("Nivel_Bateria: ");
+      Serial.println(Nivel_Bateria);
+      Serial.print("Pedal_Derecho: ");
+      Serial.println(Pedal_Derecho);
+
+
   }
-  if(inString.length()>0)
-  {
-    //Serial.println(inString);
-    String potval;
-    potval= inString.substring(9,12);
-    Serial.println(potval);
-  
-  }
+
+  // mandar tiempo hacia la pantalla
+ /* sprintf(buf, "t1.txt=\"%d:%d:%d\"", horas, mins, seg);
+  Serial.print(buf);
+  Serial.write(0xff);
+  Serial.write(0xff);
+  Serial.write(0xff);
+
+  Serial.print("j2.val=");
+  Serial.print(temp1);
+  Serial.write(0xff);
+  Serial.write(0xff);
+  Serial.write(0xff);
+
+  Serial.print("j3.val=");
+  Serial.print(temp2);
+  Serial.write(0xff);
+  Serial.write(0xff);
+  Serial.write(0xff);
+
+  Serial.print("j4.val=");
+  Serial.print(temp3);
+  Serial.write(0xff);
+  Serial.write(0xff);
+  Serial.write(0xff);
+
+  Serial.print("j0.val=");
+  Serial.print(Nivel_Bateria);
+  Serial.write(0xff);
+  Serial.write(0xff);
+  Serial.write(0xff);
+
+  Serial.print("j1.val=");
+  Serial.print(Pedal_Derecho);
+  Serial.write(0xff);
+  Serial.write(0xff);
+  Serial.write(0xff);*/
+
+
+
+
   //delay(100);
 }
